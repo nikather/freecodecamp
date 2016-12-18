9db0c34abc0396f9b9a0c7ce3d2fe46b39b096c5
echavarria <aaronechavarria@gmail.com>
2016-12-17 17:24:44 -0800

    Spinal Case
    pset fin

--- FrontEnd_Cert/Problem_Sets/5_intermediate_algo_scripting.js
+++ FrontEnd_Cert/Problem_Sets/5_intermediate_algo_scripting.js
@@ -123,3 +123,14 @@ function spinalCase(str) {
 }
 
 spinalCase('This Is Spinal Tap');
+
+function spinalCase(str) {
+  // Create a variable for the white space and underscores.
+  var regex = /\s+|_+/g;
+
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  // Replace space and underscore with -
+  return str.replace(regex, '-').toLowerCase();
+}
